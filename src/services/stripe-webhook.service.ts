40d4e6350397ccdb5e3501703e6644e4d13cf422
Mac <apple@Apples-MacBook-Pro.local>
2024-04-01 12:41:56 +0530

    log

--- src/services/stripe-webhook.service.ts
+++ src/services/stripe-webhook.service.ts
@@ -33,13 +33,14 @@ export class StripeWebhookService {
                 const sig = request.headers['stripe-signature'];
 
                 let event;
-
+         
                 try {
                     event = stripe.webhooks.constructEvent(
                         request.body,
                         sig,
                         endpointSecret,
                     );
+                    console.log("-------------event---------",event);
                 } catch (err) {
                     this.logger.error(`Webhook Error: ${err.message}`);
                     response.status(400).send(`Webhook Error: ${err.message}`);
@@ -48,7 +49,9 @@ export class StripeWebhookService {
 
                 switch (event.type) {
                     case 'charge.succeeded':
+                        console.log("-------------------event.type---------------",event.type)
                         const amount = event.data.object.amount_captured;
+                        console.log("-------------------amount---------------",amount)
                         const formattedNumber = `${amount
                             .toString()
                             .slice(0, -2)}.${amount.toString().slice(-2)}`;
